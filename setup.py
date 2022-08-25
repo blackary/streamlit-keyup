e204c65b1a94da6bcada53bcf0465828dc0ac894
Zachary Blackwood <zachary@streamlit.io>
2022-08-25 17:02:37 -0400

    Correct RELEASE flag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-keyup",
-    version="0.1.0",
+    version="0.1.1",
     author="Zachary Blackwood",
     author_email="zachary@streamlit.io",
     description="Text input that renders on keyup",
